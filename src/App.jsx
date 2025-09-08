527b28a4492adeae1c48c945358d3807e273abb8
SalisDev <alissongselau@gmail.com>
2025-09-08 18:23:57 -0300

    comit inicial

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Estudar programação',
+      description: 'Revisar conceitos de React, JavaScript e CSS.',
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: 'Fazer compras',
+      description: 'Comprar leite, pão, ovos e frutas.',
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: 'Ler um livro',
+      description: 'Ler pelo menos 30 páginas do livro.',
+      isCompleted: false,
+    },
+    {
+      id: 4,
+      title: 'Exercitar-se',
+      description: 'Fazer 30 minutos de caminhada ou treino em casa.',
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTask = tasks.map((task) => {
+      // preciso atualizar a tarefa
+      if (task.id == taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      //  não precisa atualizar
+
+      return task;
+    });
+
+    setTasks(newTask);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTask = tasks.filter((task) => task.id != taskId);
+    setTasks(newTask);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: tasks.length + 1,
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
